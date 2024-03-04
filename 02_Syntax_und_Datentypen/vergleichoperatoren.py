51348f1725ffaf33e3941e9578b72b29a15c500f
jimmy Daume <160715220+Daumejimmy@users.noreply.github.com>
2024-03-04 16:12:44 +0100

    Create vergleichoperatoren.py

---
+++ 02_Syntax_und_Datentypen/vergleichoperatoren.py
@@ -0,0 +1,40 @@
+a = 5
+b = 10
+c = 15
+
+# Vergleiche, ob a gleich b ist
+print(a == b)  # Gibt False aus, da 5 nicht gleich 10 ist
+
+# Vergleiche, ob a ungleich b ist
+print(a != b)  # Gibt True aus, da 5 nicht gleich 10 ist.
+
+# Vergleiche, ob a größer als b ist
+print(a > b)  # Gibt False aus, da 5 nicht größer als 10 ist
+
+# Vergleiche, ob a kleiner als b ist
+print(a < b)  # Gibt True aus, da 5 kleiner als 10 ist
+
+# Vergleiche, ob a größer oder gleich b ist
+print(a >= b)  # Gibt False aus, da 5 nicht größer oder gleich 10 ist
+
+# Vergleiche, ob a kleiner oder gleich b ist
+print(a <= b)  # Gibt True aus, da 5 kleiner oder gleich 10 ist
+
+# Bedingung: Wenn a kleiner als b und b größer als c ist, dann gebe den Text aus
+if a < b and b > c:
+    print("b ist größer als a und c")
+
+
+
+Temperatur = 32
+
+# Überprüfe, ob die Temperatur über 30°C liegt
+if Temperatur > 30:
+    print("Alarm! Die Temperatur übersteigt 30°C.")
+
+# Überprüfe, ob die Temperatur unter oder gleich 10°C liegt
+if Temperatur <= 10:
+    print("Alarm! Die Temperatur ist sehr niedrig, unter oder gleich 10°C.")
+
+
+    
